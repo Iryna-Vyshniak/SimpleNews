404a74bfbe636b6e5bb68d4bb1369476973b82e5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-24 00:54:13 +0300

    use fetch instead axios

--- services/newsApi.js
+++ services/newsApi.js
@@ -1,14 +1,11 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://api.nytimes.com/svc/topstories/v2';
+const BASE_URL = 'https://api.nytimes.com/svc/topstories/v2';
 const API_KEY = 'ai892Aao64TMeZOgTj6Bbw8g6062KVFI';
 
-export const getTopNews = async () => {
-  try {
-    const data = await axios.get(`/arts.json?api-key=${API_KEY}`);
-    console.log('DATA', data);
-    return data;
-  } catch (error) {
-    throw new Error('Oops, there is no articles');
-  }
+export const getTopNews = () => {
+  return fetch(`${BASE_URL}/arts.json?api-key=${API_KEY}`).then((response) => {
+    if (response.status === 404) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 };
