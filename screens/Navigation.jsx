dc7738f6ab76c6d9a1245f3b919de26a07f82239
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-24 23:22:10 +0300

    add Stack.Screen for DetailsPost

--- screens/Navigation.jsx
+++ screens/Navigation.jsx
@@ -1,20 +1,18 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Home } from './Home';
 import DetailsPost from './DetailsPost';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Home' component={Home} options={{ title: 'Bestsellers' }} />
-        <Stack.Screen
-          name='DetailsPost'
-          component={DetailsPost}
-          options={{ title: 'About Book' }}
-        />
+        <Stack.Screen name='DetailsPost' component={DetailsPost} options={{ title: 'Details' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
